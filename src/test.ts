c0b175fb9fd9afffd19c3978b269a5528d47dfdc
Luke Hardiman <luke@hardiman.co.nz>
2018-04-22 19:25:09 +1200

    updated test and readme

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,12 @@
 import { abash } from '../abash';
 import { expect } from 'chai';
 
+describe('Constructor Test', () => {
+    it('throw if cipher & decipher strings are not the same length', () => {
+        expect(function () { const result = new abash('abcdefghijklmnopqrstuvwxyz', '123'); }).to.throw('cipher string & decipher string must be the same string length');
+    });
+});
+
 describe('Example 1', () => {
     it('should return car', () => {
         const result = new abash('abcdefghijklmnopqrstuvwxyz', 'zodvqukgwefbyitmrsplhacxnj');
@@ -14,3 +20,10 @@ describe('Example 2', () => {
         expect(result.decipher('skd qj qucbjvq?')).to.equal('why so serious?');
     });
 });
+
+describe('The challenge', () => {
+    it('should return houston, we have a problem', () => {
+      const result = new abash('abcdefghijklmnopqrstuvwxyz','oephjizkxdawubnytvfglqsrcm');
+      expect(result.decipher('knlfgnb, sj koqj o yvnewju')).to.equal('houston, we have a problem');
+    });
+  });
